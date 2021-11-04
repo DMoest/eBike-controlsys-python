3b84df1814ba9162f5bae7ccff4b765f09798744
Robert Israelsson <israelsson.r@gmail.com>
2021-11-04 21:04:27 +0100

    Switched to use commandline argument for number of bikes instead of input

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from multiprocessing import Process
 from bike_controller import BikeController
+import sys
 
 def main():
     """
     Initializes and runs a new BikeController.
     """
+    NUM_BIKES = int(sys.argv[1])
+
     bike = BikeController()
-    number_of_bikes = int(input("Enter number of bikes to use in simulation: "))
-    bike.run(number_of_bikes)
+    # number_of_bikes = int(input("Enter number of bikes to use in simulation: "))
+    bike.run(NUM_BIKES)
         
 if __name__ == "__main__":
     main()
